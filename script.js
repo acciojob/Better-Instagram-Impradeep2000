603a936f0e9c761ba9558f33f026fe270ed5bc64
Pradeep Kumar <95734296+Impradeep2000@users.noreply.github.com>
2023-09-22 17:36:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,25 +1,26 @@
-//your code here
-document.addEventListener("DOMContentLoaded", function() {
-  const images = document.querySelectorAll(".image");
-  let selectedImage = null;
+function startDragging(event) {
+	event.dataTransfer.setData("text" , event.target.id);
+}
 
-  images.forEach(image => {
-    image.addEventListener("dragstart", function(e) {
-      selectedImage = this;
-    });
+function allowDrop(event) {
+	event.preventDefault();
+}
 
-    image.addEventListener("dragover", function(e) {
-      e.preventDefault();
-    });
 
-    image.addEventListener("drop", function(e) {
-      e.preventDefault();
-      if (selectedImage !== null && selectedImage !== this) {
-        // Swap background images
-        const tempImage = this.style.backgroundImage;
-        this.style.backgroundImage = selectedImage.style.backgroundImage;
-        selectedImage.style.backgroundImage = tempImage;
-      }
-    });
-  });
-});
+function dropped(event) {
+	// console.log(event.target);
+	// The Current container
+	const temp_id = event.target.id;
+	const innerText1 = event.target.innerText;
+	//The replacing container 
+	const data_id = event.dataTransfer.getData("text");
+	const container = document.getElementById(data_id);
+	const innerText2 = container.innerText;
+
+	//Swapping the container according to their id and innerText
+	container.id = temp_id;
+	container.innerText = innerText1;
+	event.target.id = data_id;
+	event.target.innerText = innerText2;
+	
+}
